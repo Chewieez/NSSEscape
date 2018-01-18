0d1aed84d8493a52ac1b20973ac20e77011600a4
Dre mf Randaci <drerandaci@gmail.com>
2018-01-18 14:36:08 -0600

    cohort class and query all cohorts

---
+++ NSSEscape/CohortClass.cs
@@ -0,0 +1,9 @@
+namespace NSSEscape
+{
+    public class Cohort
+    {
+        public int cohort_id { get; set; }
+        public int cohort_number { get; set; }
+        public string server_tech { get; set; }
+    }
+}
--- NSSEscape/ViewCohortsMenu.cs
+++ NSSEscape/ViewCohortsMenu.cs
@@ -1,16 +1,34 @@
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+
 namespace NSSEscape
 {
     public class ViewCohortsMenu
-    {  
+    {
         private DatabaseInterface db;
-       
+        private List<Cohort> _cohorts = new List<Cohort>();
         public ViewCohortsMenu(DatabaseInterface DB)
         {
             db = DB;
         }
 
-        public void Show() {
-            
+        public void Show()
+        {
+            db.Query("select * from cohort",
+                (SqliteDataReader reader) =>
+                {
+                    _cohorts.Clear();
+                    while (reader.Read())
+                    {
+                        _cohorts.Add(new Cohort()
+                        {
+                            cohort_id = reader.GetInt32(0),
+                            cohort_number = reader.GetInt32(1),
+                            server_tech = reader.ToString()
+                        });
+                    }
+                }
+            );
         }
     }
 }
